56bd41e493218724bcf11a5e055c6ee096072816
artemmmichurin <amichurin@sbdagroup.com>
2024-03-10 11:30:50 +0300

    updated EDA notebook

--- utils.py
+++ utils.py
@@ -1,22 +1,41 @@
-def faeture_drop(data):
+import gdown
+import pandas as pd
+
+
+def download_raw_data_from_drive_and_open_in_pandas(file_id="1cS6pE2ZD127iSVEiLRDzGd_b65J7GTd9",
+                                                    file_path="raw_data.parquet"):
+    # скачивает файл
+    gdown.download(id=file_id, output=file_path)
+
+    return pd.read_parquet(file_path)
+
+
+def drop_ununique_features(data):
+    # создает датафрейм только с колонками, содержащими больше одного уникального значения
     return data.copy().loc[:, data.nunique() != 1]
+
 
 def remove_highly_correlated_features(df, threshold=0.9):
     corr_matrix = df.corr().abs()
 
-    # Инициализируем множество для хранения индексов признаков, которые нужно удалить
+    # инициализируем множество для хранения индексов признаков, которые нужно удалить
     features_to_remove = set()
 
-    # Проходимся по всем элементам матрицы корреляции
+    # проходимся по всем элементам матрицы корреляции
     for i in range(len(corr_matrix.columns)):
         for j in range(i + 1, len(corr_matrix.columns)):
-            # Если корреляция между двумя столбцами выше заданного порога
+            # если корреляция между двумя столбцами выше заданного порога
             if corr_matrix.iloc[i, j] > threshold:
-                # Определяем, какой из двух признаков удалить
+                # определяем, какой из двух признаков удалить
                 colname = corr_matrix.columns[j]
                 features_to_remove.add(colname)
 
-    # Удаляем признаки
+    # удаляем признаки
     df_reduced = df.drop(columns=features_to_remove)
 
     return df_reduced
+
+
+def get_categorical_columns():
+
+    pass
